28a3114ff7750ae608d50b590462080e5b42d9dd
U-LIVINGLAB\ll_stkapate <ll_stkapate@LL-NB-08.LivingLab.local>
2020-04-28 14:18:01 +0200

    update the file with comments and clean up the code.

--- kaushal/D2L_mesurement_data_Q5/frame_D2L_A7_south_14.04.20_realSize_ROI.cpp
+++ kaushal/D2L_mesurement_data_Q5/frame_D2L_A7_south_14.04.20_realSize_ROI.cpp
@@ -8,19 +8,29 @@
 using namespace std;
 using namespace cv;
 
-#define START_X 243		// 485 // 885 // 243
-#define START_Y 260		// 380 // 260
-#define END_Y  START_Y	// 380
-#define FRAME_WIDTH 640 // 1280 // 640
-#define HSV 0
-#define PIXEL_DIST 2.27  // 2.28 //
+#define START_X 243		 // 485 // 885 // 243
+#define START_Y 260		 // 380 // 260
+#define END_Y  START_Y	 // 380
+#define FRAME_WIDTH 640  // 1280 // 640
+
+#define PIXEL_DIST 2.27  // in to mm  by calibration // 2.28 
 
 int main(int argc, char *argv[])
 {
   int iFrameCount = 0;
-  int i = 4212;
+  int i = 0;
   FILE *file;
   
+  // Parameters for ROI based on GT data 
+  int GT_D2L;
+  int pix_lane;
+  int GT_point_lane_x;
+  int GT_point_lane_y;
+  int ROI_x1;
+  int ROI_y1;
+  int ROI_x2;
+  int ROI_y2;
+
   // Variables for getting the Pixel value
   Scalar pixelValue;
   uchar pixel;
@@ -31,7 +41,7 @@ int main(int argc, char *argv[])
   int iLaneColorUpperThreshold = 250;
 
   Mat frame, resize_frame, edges, adrive_logo, new_logo;
-  Mat whiteLane, yellowLane, LinesImg, HSV_Img, gray;
+  Mat whiteLane, LinesImg, HSV_Img;
 
   // Load the image files from folder in sequence
   vector<cv::String> fn;
@@ -48,7 +58,8 @@ int main(int argc, char *argv[])
   // to save the frames as video
   VideoWriter wrOutVideo;
   const String name = "./A7_south_processing.avi";
-  wrOutVideo.open(name, VideoWriter::fourcc('M', 'J', 'P', 'G'), 10.0, Size(1280, 720), true);
+  //wrOutVideo.open(name, VideoWriter::fourcc('M', 'J', 'P', 'G'), 10.0, Size(1280, 720), true);
+  wrOutVideo.open(name, VideoWriter::fourcc('M', 'J', 'P', 'G'), 10.0, Size(640, 480), true);
 
   // Process the frames to find D2L and save the processed frames in device
   while(1)
@@ -64,10 +75,12 @@ int main(int argc, char *argv[])
 	// Load the frame to process
     //frame = imread(fn[iFrameCount].c_str(), IMREAD_COLOR);
 
+	// Calibration images 14.04.2020_A7_Q5
     //frame = imread("D:/A7_measurement_14.4.20/cal/4672_5cm.png", IMREAD_COLOR);
 	//frame = imread("D:/A7_measurement_14.4.20/cal/1035_12.5cm.png", IMREAD_COLOR);
 	//frame = imread("D:/A7_measurement_14.4.20/cal/178_47cm.png", IMREAD_COLOR);
 	
+	// Process frames ZIM 212
 	//frame = imread("D:/A7_measurement_14.4.20/img/4294.png", IMREAD_COLOR);
 	//frame = imread("D:/A7_measurement_14.4.20/img/4296.png", IMREAD_COLOR);
 	//frame = imread("D:/A7_measurement_14.4.20/img/4437.png", IMREAD_COLOR);
@@ -94,48 +107,49 @@ int main(int argc, char *argv[])
     // If the frame is empty, break immediately
     if (frame.empty()) break;
 
-	//imshow("input_img", frame);
-
+	// Resize the frame
 	//cv::resize(frame, resize_frame, Size(1280, 720));
 	//cv::resize(frame, frame, Size(1280, 720));
 
 	imshow("img", frame);
 
-	cvtColor(frame, HSV_Img, COLOR_BGR2HSV);
+	//cvtColor(frame, HSV_Img, COLOR_BGR2HSV);
 
 	//imshow("img_HSV1", HSV_Img);
 
-	int GT_D2L = 33.71 * 10;
-		
-	int pix_lane = GT_D2L / PIXEL_DIST;
-	int GT_point_lane_x = START_X + pix_lane;
-	int GT_point_lane_y = START_Y;
-
-	int ROI_x1 = GT_point_lane_x - 20;
-	int ROI_y1 = GT_point_lane_y - 20;
+	// Convert the GT_D2L in mm
+	GT_D2L = 33.71 * 10;
 	
-	int ROI_x2 = 40;
-	int ROI_y2 = 40;
+	// Convert the D2L in to pixels
+	pix_lane = GT_D2L / PIXEL_DIST;
+	
+	// GT point on Lnae (x,y)
+	GT_point_lane_x = START_X + pix_lane;
+	GT_point_lane_y = START_Y;
+
+	// create the points (x1, y1, x2, Y2) fro ROI
+	ROI_x1 = GT_point_lane_x - 20;
+	ROI_y1 = GT_point_lane_y - 20;
+	ROI_x2 = 40;
+	ROI_y2 = 40;
 	
 	printf("\nGT_D2L: %d mm, Pix_ROI: %d", GT_D2L, pix_lane);
 	
 	printf("\nROI: X1= % d, Y1= % d, x2= % d,Y2= % d", ROI_x1, ROI_y1, ROI_x2, ROI_y2);
 
-	//here we define our region of interest
-	
+	// Here we define our region of interest
 	//Rect const box(247, 50, 293, 350); // 47 cm > calibration // process_2
 
 	Rect const box(ROI_x1, ROI_y1, ROI_x2, ROI_y2); // 47 cm > calibration // process_2
 
 	Mat ROI = frame(box);
 
-	// convert our img to HSV Space
+	// convert our fame to HSV Space
 	cvtColor(ROI, HSV_Img, COLOR_BGR2HSV);
 
 	imshow("img_HSV", HSV_Img);
 
-	//white color thresholding
-
+	// white color thresholding
 	Scalar whiteMinScalar = Scalar(100, 100, 50); // 10 0 90 // 100 100 60 //
 	Scalar whiteMaxScalar = Scalar(255, 189, 255); // 50 189 255 // 250 189 255
 
@@ -149,17 +163,14 @@ int main(int argc, char *argv[])
 	Canny(LinesImg, LinesImg, minCannyThreshold, maxCannyThreshold, 3, true);
 
 	// Morphological Operation
-	
 	Mat k = getStructuringElement(MORPH_RECT, Size(5, 5)); //MATLAB :k=Ones(9) // process_1 
 
 	morphologyEx(LinesImg, LinesImg, MORPH_CLOSE, k);
 	
-	//imshow("morphology", LinesImg);
-
 	// now applying hough transform TO dETECT Lines in our image
 	vector<Vec4i> lines;
+
 	//HoughLinesP(LinesImg, lines, 1, CV_PI / 180, 150, 0, 50); // process_1
-	
 	HoughLinesP(LinesImg, lines, 1, CV_PI / 180, 5, 0, 10); // process_2
 	
 	/*
@@ -182,7 +193,6 @@ int main(int argc, char *argv[])
 		line(ROI, Point(l[0], l[1]), Point(l[2], l[3]), greenColor, 1.5, 4);
 	}
 
-#if 1
 	//imshow("ROI_Lines", ROI);
 
 	Canny(frame, LinesImg, 250, 255, 3, true);
@@ -224,44 +234,45 @@ int main(int argc, char *argv[])
 			line(frame, Point(START_X, START_Y), Point(i, END_Y), Scalar(255, 255, 0), 1, 8);
 			//line(edges, Point(START_X, END_Y), Point(i, END_Y), Scalar(255, 255, 255), 1, 8);
 
-			// put the txt on frame for information
+			// put the txt Frame number on Resize frame for information
 			//putText(frame, format("Frame: %s", fn[iFrameCount].c_str()), Point(0, 700), FONT_HERSHEY_PLAIN, 1, Scalar(255, 255, 255));
 
-			// put the txt on frame for information
-			//putText(frame, format("Frame: %s", fn[iFrameCount].c_str()), Point(0, 450), FONT_HERSHEY_PLAIN, 0.75, Scalar(255, 255, 255));
+			// put the text Frame number on real frame for information
+			putText(frame, format("Frame: %s", fn[iFrameCount].c_str()), Point(0, 450), FONT_HERSHEY_PLAIN, 0.75, Scalar(255, 255, 255));
 
-			// put the txt on frame for information
+			// put the text D2L on Resize frame for information
 			//putText(frame, format(" D2L: %f cm", distance), Point(50, 670), FONT_HERSHEY_PLAIN, 2, Scalar(255, 255, 255));
 
-			// put the txt on frame for information
+			// put the text D2L on Real frame for information
 			putText(frame, format(" D2L: %f cm", distance), Point(0, 400), FONT_HERSHEY_PLAIN, 1.5, Scalar(255, 255, 255));
 
 			// add the Adrive logo
 			adrive_logo = imread("ADrive_Logo1.png", IMREAD_COLOR);
 			//adrive_logo = imread("HS_Kempten_Logo.jpg", IMREAD_COLOR);
 
+			// Put the LOGO on resize frame
 			cv::resize(adrive_logo, new_logo, Size(135, 50));
-
-			//adrive_logo.copyTo(resize_frame(cv::Rect(100,100, adrive_logo.cols, adrive_logo.rows)));
 			//new_logo.copyTo(frame(cv::Rect(1120, 650, new_logo.cols, new_logo.rows)));
 
+			// Put the LOGO on real frame
+			new_logo.copyTo(frame(cv::Rect(500, 420, new_logo.cols, new_logo.rows)));
+			
 			// Write the process METADATA in .csv file
 			//fprintf(file, "%s, %f\n", fn[iFrameCount].c_str(), distance);
 
-			// to save frame as image
+			// save process_frame as image
 			//imwrite(ss.str().c_str(), frame);
-			imwrite("process_6472.png", frame);
+			//imwrite("process_6472.png", frame);
 
 			// Write to video file
 			//wrOutVideo.write(frame);
 			
 			// Display the original frame or process frame
-			imshow("Process_frame", frame);
+			imshow("Process_frame_D2L", frame);
 
 			break;
 		}
 	}
-#endif
 		
 	fclose(file);
 	
